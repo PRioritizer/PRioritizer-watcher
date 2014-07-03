e6d8bf5ca3ebca20cac43aae39589dbbc2f7364a
Erik van der Veen <erikvdv1@gmail.com>
2014-07-03 17:36:12 +0200

    Test case for cmd task runner

--- src/test/scala/WatcherSpec.scala
+++ src/test/scala/WatcherSpec.scala
@@ -1,12 +1,17 @@
 import org.specs2.mutable._
+import settings.TaskSettings
+import task.CommandLineRunner
 
 class WatcherSpec extends Specification {
+  val command = TaskSettings.command
+  val repoDir = TaskSettings.repositories
+
   "When watching the queue it" should {
     "execute the task correctly" in {
-      val command = "prioritizer $owner $repository $dir"
+
       val queue = new TestQueue
       val data = new TestDatabase
-      val runner = new TestRunner(command)
+      val runner = new CommandLineRunner(repoDir, command)
 
       Watcher.watch(queue, data, runner)
       true must beTrue
