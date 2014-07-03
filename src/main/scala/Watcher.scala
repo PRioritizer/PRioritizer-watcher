9b43dbe0cbc5dd53dd39f8f32063ce613ecc8cf3
Erik van der Veen <erikvdv1@gmail.com>
2014-07-03 13:15:26 +0200

    Print more info

--- src/main/scala/Watcher.scala
+++ src/main/scala/Watcher.scala
@@ -24,14 +24,16 @@ object Watcher {
       database.open()
 
       queue listen { eventId =>
-        println(s"> Event id: $eventId")
+        print(s"> Event id: $eventId")
 
         database.getPullRequest(eventId) match {
           case Success(pullReq) =>
-            println(pullReq)
-//            channel.basicAck(delivery.getEnvelope.getDeliveryTag, false)
+            print(s" base: ${pullReq.base.owner}/${pullReq.base.repo}")
+            println(s" head: ${pullReq.head.owner}/${pullReq.head.repo}")
+          //            channel.basicAck(delivery.getEnvelope.getDeliveryTag, false)
           case Failure(e) =>
-//            channel.basicNack(delivery.getEnvelope.getDeliveryTag, false, true)
+            println(s" Error - ${e.getMessage}")
+          //            channel.basicNack(delivery.getEnvelope.getDeliveryTag, false, true)
         }
       }
     } finally {
@@ -50,8 +52,8 @@ object RabbitMQSettings {
 
 object MongoDBSettings {
   lazy val host = Settings.get("mongodb.host").getOrElse("localhost")
-  lazy val username = Settings.get("mongodb.username").getOrElse("")
   lazy val port = Settings.get("mongodb.port").fold(27017)(p => p.toInt)
+  lazy val username = Settings.get("mongodb.username").getOrElse("")
   lazy val password = Settings.get("mongodb.password").getOrElse("")
   lazy val database = Settings.get("mongodb.database").getOrElse("")
   lazy val collection = Settings.get("mongodb.collection").getOrElse("")
