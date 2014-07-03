d1ca85c54ed802243a9d0be8abe23798c09b835f
Erik van der Veen <erikvdv1@gmail.com>
2014-07-03 14:45:45 +0200

    Allow to read from secondary db

--- src/main/scala/events/EventDatabase.scala
+++ src/main/scala/events/EventDatabase.scala
@@ -17,6 +17,8 @@ class EventDatabase(host: String, port: Int, username: String, password: String,
     } else {
       new MongoClient(server)
     }
+    
+    client.setReadPreference(ReadPreference.secondaryPreferred())
     database = client.getDB(databaseName)
     collection = database.getCollection(collectionName)
   }
