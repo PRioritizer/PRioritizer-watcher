5e42471cf3de71efc381f09aa08afc373944f492
Erik van der Veen <erikvdv1@gmail.com>
2014-10-08 11:22:04 +0200

    Disable settings initialization in test

--- src/test/scala/WatcherSpec.scala
+++ src/test/scala/WatcherSpec.scala
@@ -5,19 +5,19 @@ import settings.TaskSettings
 import task.CommandLineRunner
 
 class WatcherSpec extends Specification {
+/*
   val command = TaskSettings.command
   val repoDir = TaskSettings.repositories
 
   "When watching the queue it" should {
     "execute the task correctly" in {
-/*
       val queue = new TestQueue
       val data = new TestDatabase
-      val runner = new CommandLineRunner(repoDir, command, new PrintWriter(null, true))
+      val runner = new CommandLineRunner(repoDir, command, new PrintWriter(System.out, true))
 
       Watcher.watch(queue, data, runner)
-*/
       true must beTrue
     }
   }
+*/
 }
