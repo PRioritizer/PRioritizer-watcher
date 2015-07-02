e737341124ef31d911c0aec7fb97d408aececaab
Erik van der Veen <erikvdv1@gmail.com>
2015-07-02 16:09:38 +0200

    Limit thread count by 2

--- run.bat
+++ run.bat
@@ -6,6 +6,7 @@ SET VERSION=1.0
 SET SCALA_VERSION=2.11
 SET D=%~dp0
 SET JAR=%D%\target\scala-%SCALA_VERSION%\%NAME%-assembly-%VERSION%.jar
+SET THREADS=2
 
 IF NOT EXIST %JAR% GOTO JarNotFound
 GOTO ValidArgs
@@ -16,7 +17,8 @@ ECHO Make sure you run `sbt assembly` to build the JAR executable.
 GOTO end
 
 :ValidArgs
-java -jar %JAR%
+SET PROPS=-Dfile.encoding=UTF8 -Dscala.concurrent.context.minThreads=%THREAD% -Dscala.concurrent.context.numThreads=%THREADS% -Dscala.concurrent.context.maxThreads=%THREADS%
+java %PROPS% -jar %JAR%
 :end
 
 ENDLOCAL
